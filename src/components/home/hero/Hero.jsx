2664e971d965ee1de3c48931740a659d28194db3
Joshua Artis <joshartis0090@gmail.com>
2024-02-05 12:10:05 -0500

    Working Model

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -2,16 +2,20 @@ import React from "react"
 import Heading from "../../common/Heading"
 import "./hero.css"
 
+
 const Hero = () => {
+  function displaySliderValue(eSlider){   
+    eSlider.parentElement.querySelector('span').textContent = eSlider.value;
+  }
   return (
     <>
       <section className='hero'>
         <div className='container'>
-          <Heading title='Search Your Next Venue ' subtitle='Find new & featured spaces located in your city.' />
+          <Heading title='Find Your Next Venue ' subtitle='New & featured spaces located in your city.' />
 
           <form className='flex'>
             <div className='box'>
-              <span>City/Street</span>
+              <span>City</span>
               <input type='text' placeholder='Location' />
             </div>
             <div className='box'>
@@ -19,8 +23,9 @@ const Hero = () => {
               <input type='text' placeholder='Property Type' />
             </div>
             <div className='box'>
-              <span>Price Range</span>
-              <input type='text' placeholder='Price Range' />
+              <span>Budget</span>
+              <span>1</span>
+              <input type='range' value="1" min="0" max="30000" onchange="displaySliderValue(this);"/>
             </div>
             <div className='box'>
               <h4>Advance Filter</h4>
